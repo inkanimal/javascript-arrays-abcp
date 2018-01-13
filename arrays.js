af6bfe103a963b2b668a6d6a04fad2396983bc5a
geoffrey woodcock <theatomproject@gmail.com>
2018-01-13 23:00:50 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -27,6 +27,6 @@ function removeElementFromBeginningOfArray(array){
   return array
 }
 function destructivelyRemoveElementFromEndOfArray(array){
-  array.push(1)
+  array = array.push(1)
   return array
 }
