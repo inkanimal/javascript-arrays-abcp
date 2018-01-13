2572f4069f41378b528994342fcdd721966ef838
geoffrey woodcock <theatomproject@gmail.com>
2018-01-13 22:35:25 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,6 +1,6 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
 function addElementToBeginningOfArray(array, element){
-  return chocolateBars.upshift("milky way")
+  return chocolateBars[0] = "foo"
 }
 function destructivelyAddElementToBeginningOfArray(array, element){
 
