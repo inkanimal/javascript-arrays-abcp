eff53e0c8dc73a15eadb99296f5e619bb61d1027
geoffrey woodcock <theatomproject@gmail.com>
2018-01-13 22:23:02 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,1 +1,10 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
+function addElementToBeginningOfArray(array, element){
+
+}
+function destructivelyAddElementToBeginningOfArray(array, element){
+
+}
+function accessElementInArray(array, index){
+  return chocolateBars[2]
+}
